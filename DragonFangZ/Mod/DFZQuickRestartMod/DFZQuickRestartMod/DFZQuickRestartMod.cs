d076b061196b2e5421c0fef5c7fac184d3d09b6d
yagamuu <ygmpeer@gmail.com>
2024-01-29 11:10:09 +0900

    Add DFZQuickRestartMod

---
+++ DragonFangZ/Mod/DFZQuickRestartMod/DFZQuickRestartMod/DFZQuickRestartMod.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace DFZQuickRestartMod
+{
+    public class DFZQuickRestartMod : MelonMod
+    {
+        public override void OnUpdate()
+        {
+            GameScene gameScene = GameObject.Find("GameScene").GetComponent<GameScene>();
+            if (Input.GetKeyDown(KeyCode.F5) && gameScene.Panel.CanControl()) {
+                StateSerializer.DeleteState(0);
+                Browser.GoTo(string.Format("Game?dungeon={0}", gameScene.CurrentStartDungeonParam.DungeonId), new PageOption
+                {
+                    Fade = true,
+                    ClearHistory = true
+                });
+            }
+        }
+    }
+}
