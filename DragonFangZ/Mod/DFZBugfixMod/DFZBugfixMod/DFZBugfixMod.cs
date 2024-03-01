21a9a07d7e5c61abe7ee6fec91f50c927fb7ae02
HARADA Makoto <haramako@gmail.com>
2024-03-02 01:21:57 +0900

    Fix bug: protection souls doesn't works when multiple protection souls.

--- DragonFangZ/Mod/DFZBugfixMod/DFZBugfixMod/DFZBugfixMod.cs
+++ DragonFangZ/Mod/DFZBugfixMod/DFZBugfixMod/DFZBugfixMod.cs
@@ -40,5 +40,52 @@ namespace DFZBugrfixMod
 				}
 			}
 		}
+
+
+		[HarmonyPatch(typeof(Game.FieldAction.CharacterSoul), "CheckSoulFilter")]
+		public static class PatchProtectSouls
+        {
+			public static bool Prefix(ref bool __result, Field f, Character c, SoulCondType cond, FilterOption opt)
+            {
+				__result = CheckSoulFilterPatched(f, c, cond, opt);
+				return false;
+			}
+
+			public static bool CheckSoulFilterPatched(Field f, Character c, SoulCondType cond, FilterOption opt)
+			{
+				if (c.IsStatusActive(CharacterStatus.Seal))
+				{
+					return false;
+				}
+
+				var souls = c.Souls;
+				var len = souls.Count;
+				for (int i = 0; i < len; i++)
+				{
+					var soul = souls[i];
+					bool invoke = false;
+					if (soul.Type == cond && c.IsSoulActive(f, soul))
+					{
+						if (opt.Certain)
+						{
+							return true;
+						}
+
+						if (opt.Status != CharacterStatus.NoneCharacterStatus)
+						{
+							if (soul.Status.Contains(opt.Status))
+							{
+								return true;
+							}
+						}
+						else
+						{
+							invoke = true;
+						}
+					}
+				}
+				return false;
+			}
+		}
 	}
 }
