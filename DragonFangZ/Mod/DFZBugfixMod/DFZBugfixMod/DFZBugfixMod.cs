b34a8bf61e8f91d5dd9b3e7eaaa77d3cc96e6973
HARADA Makoto <haramako@gmail.com>
2024-03-01 21:55:32 +0900

    Add DFZBugFixMod project. Fix bug that rose doesn't attack to enemy on water.

---
+++ DragonFangZ/Mod/DFZBugfixMod/DFZBugfixMod/DFZBugfixMod.cs
@@ -0,0 +1,44 @@
+﻿using MelonLoader;
+using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using Game.Specials;
+using Game;
+using GameLog;
+using Master;
+using Game.Thinkings;
+
+namespace DFZBugrfixMod
+{
+	public class DFZBugrfixMod : MelonMod
+	{
+		[HarmonyPatch(typeof(Game.Thinkings.FindTarget), "Think")]
+		public static class Patch
+		{
+			static MoveType savedMoveType;
+
+			public static void Prefix(Field f, ThinkingCode code, Character c, Context ctx)
+			{
+				if (c.IsPlayer && code.Find == ThinkingCode.Types.FindType.Monster)
+				{
+					savedMoveType = c.T.MoveType;
+					c.T.MoveType = MoveType.Fly;
+					c.SetDirty();
+				}
+
+			}
+
+			public static void Postfix(Field f, ThinkingCode code, Character c, Context ctx)
+			{
+				if (c.IsPlayer && code.Find == ThinkingCode.Types.FindType.Monster)
+				{
+					c.T.MoveType = savedMoveType;
+					c.SetDirty();
+				}
+			}
+		}
+	}
+}
---
+++ DragonFangZ/Mod/DFZBugfixMod/README.md
@@ -0,0 +1,24 @@
+# DFZ Bugfix Mod
+
+ドラゴンファングZにていくつかのバグを修正するMODです。
+
+修正されるのは、以下のバグです。
+
+- 自動プレイ時に、ロゼが水上にいるモンスターに攻撃しない
+
+## 当Modを導入する目的
+- 上記4名の会話シーンはゲーム起動後に一度見ると以降発生しない
+- ただし、ゲームを終了すると再度発生する
+- そのため極限までRTAのタイムを短縮したい場合、毎回一度上記ボスを撃破してからRTAを開始する必要があり、手間がかかる
+- 上記の手間を省くために使用する
+
+## 使い方
+1. MelonLoaderのインストーラーを[ダウンロードする](https://github.com/LavaGang/MelonLoader.Installer/releases/latest/download/MelonLoader.Installer.exe)
+
+2. ダウンロードしたインストーラーを起動し、`Unity Game:`の横の`SELECT`ボタンを押し、ドラゴンファングZのexeファイルを選択する(インストール先はSteam内でプロパティ→ローカルファイルを閲覧などで確認できる)
+
+3. Version横の`Latest`チェックマークを外し、`v0.5.7`を選択する
+
+4. 一番下の`INSTALL`ボタンを押す
+
+5. ゲームフォルダ内のModsフォルダに[`DFZBugfix.dll`](https://github.com/yagamuu/speedrun/blob/master/DragonFangZ/Mod/DFZBugfix/DFZBugfixMod.dll)を入れる
